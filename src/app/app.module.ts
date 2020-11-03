70acd80c0090c31fc99202869ef5dc5bdaf30cee
eugenek45 <eugenekiprotich0@gmail.com>
2020-11-03 22:49:28 +0300

    created new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    UserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
